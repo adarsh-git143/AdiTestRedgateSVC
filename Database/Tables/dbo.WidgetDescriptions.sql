ee919ba293ecf822874af6db8a19eadca4f6f4e3
adarsh-git143 <adarsh.hampi@gmail.com>
2019-12-16 19:04:13 +0530

    remove Directory

--- Database/Tables/dbo.WidgetDescriptions.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[WidgetDescriptions]
-(
-[WidgetID] [int] NOT NULL IDENTITY(1, 1),
-[Description] [varchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[WidgetName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
-) ON [PRIMARY]
-GO
